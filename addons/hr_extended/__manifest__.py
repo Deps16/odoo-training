a6bcbbd7e3ba3d92fcf9a6690d964f38b6a440e8
Daffa_telyu <daffa2831@gmail.com>
2026-06-04 20:03:54 +0700

    first blood

---
+++ addons/hr_extended/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'HR Employee Extended',
+    'version': '17.0.1.0.0',
+    'summary': 'Mewarisi dan memodifikasi modul HR Employee 17',
+    'category': 'Human Resources',
+    'author': 'Daps',
+    'depends': ['hr'], 
+    'data': [
+        'views/hr_employee_inherit_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
